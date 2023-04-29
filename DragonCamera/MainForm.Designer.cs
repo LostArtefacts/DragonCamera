94606e7a22512ce7ce5ca012192637b2383313bb
lahm86 <33758420+lahm86@users.noreply.github.com>
2023-04-29 13:59:20 +0100

    Initial commit

---
+++ DragonCamera/MainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DragonCamera
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label2 = new System.Windows.Forms.Label();
+            this.lvlTextBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.savedLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Custom level";
+            // 
+            // lvlTextBox
+            // 
+            this.lvlTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lvlTextBox.Location = new System.Drawing.Point(86, 39);
+            this.lvlTextBox.Name = "lvlTextBox";
+            this.lvlTextBox.Size = new System.Drawing.Size(487, 20);
+            this.lvlTextBox.TabIndex = 2;
+            this.lvlTextBox.TextChanged += new System.EventHandler(this.LvlTextBox_TextChanged);
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(579, 38);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 3;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Enabled = false;
+            this.saveButton.Location = new System.Drawing.Point(579, 80);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(499, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select your custom level and click Save to import the required cinematic frames f" +
+    "or the dagger cutscene.";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "TR2 Levels|*.tr2";
+            // 
+            // savedLabel
+            // 
+            this.savedLabel.AutoSize = true;
+            this.savedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.savedLabel.ForeColor = System.Drawing.Color.Green;
+            this.savedLabel.Location = new System.Drawing.Point(517, 83);
+            this.savedLabel.Name = "savedLabel";
+            this.savedLabel.Size = new System.Drawing.Size(56, 16);
+            this.savedLabel.TabIndex = 5;
+            this.savedLabel.Text = "Saved!";
+            this.savedLabel.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 115);
+            this.Controls.Add(this.savedLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.lvlTextBox);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dragon Cinematic Frames";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lvlTextBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label savedLabel;
+    }
+}
+
---
+++ DragonCamera/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using TRLevelReader;
+using TRLevelReader.Model;
+
+namespace DragonCamera
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                lvlTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void LvlTextBox_TextChanged(object sender, EventArgs e)
+        {
+            saveButton.Enabled = lvlTextBox.Text.Trim().Length > 0;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                savedLabel.Visible = false;
+
+                string path = lvlTextBox.Text.Trim();
+                if (!File.Exists(path))
+                {
+                    throw new IOException(path + " does not exist.");
+                }
+
+                TR2Level level = new TR2LevelReader().ReadLevel(path);
+                level.CinematicFrames = JsonConvert.DeserializeObject<TRCinematicFrame[]>(File.ReadAllText("frames.json"));
+                level.NumCinematicFrames = (ushort)level.CinematicFrames.Length;
+                new TR2LevelWriter().WriteLevelToFile(level, path);
+
+                savedLabel.Visible = true;
+                new Thread(HideLabel).Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void HideLabel()
+        {
+            Thread.Sleep(5000);
+            Invoke(new Action(() => savedLabel.Visible = false));
+        }
+    }
+}
